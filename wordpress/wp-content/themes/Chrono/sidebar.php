a5a262dc5863612b4de4fe79d7b21a01e048bab7
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2019-06-03 12:19:37 +0900

    Removed unnecessary themes and var_dump wp_filter.

--- wordpress/wp-content/themes/Chrono/sidebar.php
+++
@@ -1,5 +0,0 @@
-<!-- Sidebar -->
-<aside class="sidebar text-center" id="sidebar"  role="complementary" itemscope="itemscope" itemtype="https://schema.org/WPSideBar">
-  <!-- ウィジェットによるサイドバー -->
-  <?php dynamic_sidebar( 'side-widget' ); ?><!-- <h4><li>タグが隠れている -->
-</aside>
--- wordpress/wp-content/themes/wphierarchy/sidebar-splash.php
+++
@@ -1,3 +0,0 @@
-<aside id="secondary" class="widget-area" role="complementary">
-  <p>Sell somthing major!</p>
-</aside>
